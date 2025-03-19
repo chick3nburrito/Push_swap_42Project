b4e5905baec4687ba91e78daaed3f33dae932f31
Brahim Amdjar <bamdjar@c3r9p8.1337.ma>
2025-03-19 03:30:01 +0000

    instruction

---
+++ instruction.c
@@ -0,0 +1,26 @@
+# include "push_swap.h"
+
+void    ra(t_stack **stack)
+{
+        rotate(stack);
+        write(1, "ra\n", 3);
+}
+
+void    rb(t_stack **stack)
+{
+        rotate(stack);
+        write(1, "rb\n", 3);
+}
+
+void    rr(t_stack **stack_2, t_stack **stack_1)
+{
+        rotate(stack_1);
+        rotate(stack_2);
+        write(1, "rr\n", 3);
+}
+
+void    rra(t_stack **stack)
+{
+        reverse_rotate(stack);
+        write(1, "rra\n", 4);
+}
